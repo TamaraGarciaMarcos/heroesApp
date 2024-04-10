56e86ade80e716cecf67741a93eac4863c5329de
Tamara García <tamaragarciamarcos@gmail.com>
2024-04-10 17:14:39 +0200

    fin de la seccion

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -4,9 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: [
-
+    `
+    mat-card {
+      background-color: #D0ECE7;
+    }
+    `
   ]
 })
 export class NewPageComponent {
 
+  public publishers = [
+    {id: 'DC Comics', desc: 'DC - Comics'},
+    {id: 'Marvel Comics', desc: 'Marvel - Comics'}
+  ];
+
 }
+
+
