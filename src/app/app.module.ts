c4b3b2878789b443eafc5163cb3d0fef81626c4c
Tamara García <tamaragarciamarcos@gmail.com>
2024-04-10 09:18:51 +0200

    cmprobacion de instalaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     SharedModule
   ],
   providers: [
-    provideClientHydration(),
     provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
