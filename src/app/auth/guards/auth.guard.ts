464781b5b0bfce91f920f1b9add38ce3d9890b05
Tamara García <tamaragarciamarcos@gmail.com>
2024-04-12 08:55:51 +0200

    fin configuracion guards de autenticacion

---
+++ src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,44 @@
+
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+
+@Injectable({providedIn: 'root'})
+export class AuhtGuard implements CanMatch , CanActivate {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+
+    return this.authService.checkAuthentication()
+      .pipe(
+        tap(isAuthenticated => console.log('Authenticated:', isAuthenticated)),
+        tap( isAuthenticated =>{
+          if (!isAuthenticated ){
+            this.router.navigate(['./auth/login'])
+          }
+        }),
+      )
+
+  }
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    // console.log('Can Match')
+    // console.log({route, segments})
+    return this.checkAuthStatus()
+  }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    // console.log('can Activate')
+    // console.log({route, state})
+
+    return this.checkAuthStatus()
+  }
+
+
+}
